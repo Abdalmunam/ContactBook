b98ecff51a2c3d89c1c692ed55983de22a6beb48
Abdalmunam Abdalla <abdalmenam1980@gmail.com>
2023-10-06 07:25:01 +0200

    Add model Contact

---
+++ ContactBook.Api/DAL/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using ContactBook.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactBook.Api.DAL
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        
+        public DbSet<Contact> contacts { get; set; }
+    }
+}

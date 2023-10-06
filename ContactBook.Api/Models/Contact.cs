8262e29cb7189cbbcbe7540bdda8b1f4d0354146
Abdalmunam Abdalla <abdalmenam1980@gmail.com>
2023-10-06 07:10:21 +0200

    Add project files.

---
+++ ContactBook.Api/Models/Contact.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContactBook.Api.Models
+{
+    public class Contact
+    {
+        [Key]
+        public int Id { get; set; }
+        [StringLength(100)]
+        public string Name { get; set; } = string.Empty;
+        [StringLength(100)]
+        public string Email { get; set; } = string.Empty;
+        [StringLength(100)]
+        public string PhoneNumber { get; set; } = string.Empty;
+    }
+}
